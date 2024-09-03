7ad4a9b610ade18b57b73b2d3595dd581a6371a3
Thrinadh Matta <149357801+trinadhmatta2123@users.noreply.github.com>
2024-09-03 23:50:02 +0530

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Domestic from './components/Domestic';
+import International from './components/International';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/domestic" element={<Domestic />} />
+        <Route path="/international" element={<International />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
